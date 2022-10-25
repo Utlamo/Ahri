8c02fe995e18a6b6a9a2652b8e252e969a1119d7
Utlamo <danielkasiurak1@gmail.com>
2022-10-25 08:30:20 +0200

    load local files

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("code can activated");
+import chalk from 'chalk';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+import fs from 'fs';
+//const dirFiles = require('./load_modules/dir.js');
+const folderPath = __dirname + "\\ahri_modules";
+var filejs = [];
+var filepy = [];
+var filejson = [];
+
+function dirFiles(folder) {
+  let filedir = [];
+  let file = [];
+  fs.readdirSync(folder).forEach( filename => {
+    file.push(filename);
+    switch (path.extname(filename)) {
+      case ".js":
+        filejs.push(folder + "\\" + filename);
+        break;
+      case ".py":
+        filepy.push(folder + "\\" + filename);
+        break;
+      case ".json":
+        filejson.push(folder + "\\" + filename);
+        break;
+      case "":
+        filedir.push(folder + "\\" + filename);
+        break;
+      default:
+        console.error(chalk.yellow("Plik ") + chalk.red(folder + "\\" + filename) + chalk.yellow(" ma nieprawidłowe rozszerzenie i nie został załadowany!"));
+    }
+  });
+  filedir.forEach(subdir => { dirFiles(subdir); });
+}
+
+dirFiles(folderPath);
+console.log("załadowano pliki js: " + filejs);
+console.log("załadowano pliki py: " + filepy);
